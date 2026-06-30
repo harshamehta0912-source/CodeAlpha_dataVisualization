f036d292468dd2d1befaf02ea108b714733bd178
harshamehta0912-source <harshamehta0912@gmail.com>
2026-06-30 13:07:45 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("My Dashboard")
+
+st.image("sales_by_category.png")
+st.image("sales_by_region.png")
+st.image("profit_by_category.png")
+st.image("sales_vs_profit.png")
+st.image("sales_boxplot.png")
+st.image("correlation_heatmap.png")
